897437994d532f7e279f2bd082e32064f5b962bd
Guépard anonyme <evelyne.akopyan@gmail.com>
2018-03-07 15:18:48 +0100

    Create javascript_cours2.js
    Création d'une classe objet

---
+++ javascript_cours2.js
@@ -0,0 +1,8 @@
+// Création d'une classe
+class Machine {
+    constructor(){
+        this.utilite = "rien"
+    }
+}
+
+let machine = new Machine()     // machine : [object Object]: {utilite: "rien"}
