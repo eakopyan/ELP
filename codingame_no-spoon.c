7e33e33419ea37068578ed7e568b98f011025e8b
Guépard anonyme <evelyne.akopyan@gmail.com>
2018-03-10 18:35:26 +0100

    Update codingame_no-spoon.c
    Think about a while() loop instead of for()

--- codingame_no-spoon.c
+++ codingame_no-spoon.c
@@ -6,6 +6,7 @@ int main()
     scanf("%d", &height); fgetc(stdin);
     
     int x1, y1, x2, y2, x3, y3;
+    int nextr, nextb;
     char *tab[height];
     
     for (int a = 0; a < height; a++) {
@@ -16,7 +17,7 @@ int main()
     }
     
     for(int i = 0; i<height; i++) {
-        for (int j = 0; j<width; j++) {
+        for (int j = 0; j<strlen(tab[i]); j++) {
             switch(tab[i][j]) {
                 case '.' : // empty cell
                 break;
@@ -24,14 +25,20 @@ int main()
                 case '0' : // power cell
                 x1 = j;
                 y1 = i;
-                if((j==width-1)||(tab[i][j+1] == '.')) { // out of range or empty cell on the right
+                nextr = 1;
+                nextb = 1;
+                if((j==strlen(tab[i])-1)||(tab[i][j+1] != '0')) { // out of range or empty cell on the right
+                    while(j<strlen(tab[i])) {
+                        if(tab[i][j+1] != '0') j++;
+                        
+                    }
                     x2 = -1;
                     y2 = -1;
                 } else { // power cell on the right
                     x2 = j+1;
                     y2 = i;
                 }
-                if((i==height-1)||(tab[i+1][j] == '.')) { // out of range or empty cell below
+                if((i==height-1)||(tab[i+1][j] != '0')) { // out of range or empty cell below
                     x3 = -1;
                     y3 = -1;
                 } else { // power cell below
