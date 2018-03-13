31dc53b490018c67ae7c7667628dbdc32cc14526
Guépard anonyme <evelyne.akopyan@gmail.com>
2018-03-13 15:49:30 +0100

    Update Haskell_cours1.hs

--- Haskell_cours1.hs
+++ Haskell_cours1.hs
@@ -1,18 +1,39 @@
 import System.Environment -- for getArgs
 
+-- Vérification de l'ordre dans une liste
 isSorted lst = case lst of
-                 [] -> True
+                 [] -> True  -- flèche simple pour définir une association ( => pour indiquer le contexte)
                  [x] -> True
                  (x:xs@(y:ys)) -> (x <= y) && (isSorted xs)  
-                
-dupli :: [a] -> [a]                
-                
+   
+-- Duplication du premier élément d'une liste   
+dupli :: [a] -> [a]       -- fonction qui prend en param un tableau et renvoie un tableau                         
 dupli lst = case lst of
                 [] -> []
                 (x:xs) -> x:(x:(dupli xs))
+                
+addElemList x c lst
+  | c <= 0 = lst  
+  | c > 0 = addElemList x (c-1) (x:lst)
+                
+repli :: [a] -> Int -> [a]
+repli lst c = case (lst, c) of
+                ([], c) -> []
+                ((x:xs), c) -> addElemList x c (repli xs c)
+            
+-- Supprimer deux éléments successifs d'une liste           
+compress :: (Eq a) => [a] -> [a]
+compress lst = case lst of
+                [] -> []
+                [x] -> [x]
+                (x:x':xs)
+                  | x == x' -> compress (x':xs)  -- SI x = x' ALORS ....
+                  | otherwise -> x:(compress(x':xs))  -- SINON on ajoute x au résultat de compress(x':xs)
+                  
 
+-- fonction main du programme Haskell
 main = do
   args <- getArgs
-  putStrLn ( (show args)
+  putStrLn ( (show args)  -- affichage sur le terminal
              ++ " is sorted ? "
              ++ show (isSorted args) )
