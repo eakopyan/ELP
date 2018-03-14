a9a1f9a0bfee0b3c336b9af310337c8a1b65ff7a
Guépard anonyme <evelyne.akopyan@gmail.com>
2018-03-14 08:26:29 +0100

    Create javascript_cours4.js
    Promesses

---
+++ javascript_cours4.js
@@ -0,0 +1,7 @@
+/** 
+Le multithread étend l'idée du multitâche aux applications, de sorte que vous pouvez sous-diviser des opérations spécifiques au 
+sein d'une même application en threads individuels. Chaque thread peut fonctionner en parallèle. Le système d'exploitation divise 
+le temps de traitement non seulement entre différentes applications, mais aussi entre chaque thread dans une même application.
+Promesses : objet qui peut produire une valeur unique dans le futur. Une promesse doit être dans l'un des 3 états : accomplie,
+rejetée, en attente. Elle est lancée dès sa création.
+*/
